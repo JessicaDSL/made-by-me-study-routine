b3db974efb5a7787e20d94457a904742d1e7e6ca
Jessica Lima <jessikurt@gmail.com>
2023-06-29 09:15:15 -0300

    create counter project with javascript vanilla

---
+++ javascript/counter/main.js
@@ -0,0 +1,37 @@
+const counter = document.querySelector(".counter");
+const btnDecrease = document.querySelector(".decrease");
+const btnReset = document.querySelector(".reset");
+const btnIncrease = document.querySelector(".increase");
+let count = Number(counter.innerHTML)
+
+//count == 0 ? counter.style.color = 'blue' :  counter.style.color = 'red'
+
+
+
+function decreaseCounter() {
+  if( count < 0 ) counter.style.color = 'red'
+  counter.innerHTML = count - 1
+  
+  //count < 0 ? counter.style.color = 'blue' :  counter.style.color = 'red'
+  return count--
+}
+
+function resetCounter() {
+  if( count = 0 ) counter.style.color = 'green'
+  counter.innerHTML = 0
+  
+  return count = 0;
+}
+
+function increaseCounter() {
+  if( count > 0 ) counter.style.color = 'blue'
+  counter.innerHTML = count + 1
+  return count++
+}
+
+console.log(count);
+btnDecrease.addEventListener('click', decreaseCounter);
+btnReset.addEventListener('click', resetCounter);
+btnIncrease.addEventListener('click', increaseCounter);
+
+
